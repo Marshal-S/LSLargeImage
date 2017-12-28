90daeafb601963cc4b0f34503d52b72e8bf7184a
Marshal-S <921871381@qq.com>
2017-12-28 09:58:54 +0800

    Initial Commit

---
+++ LSLargeImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSLargeImage
+//
+//  Created by Marshal on 2017/12/28.
+//  Copyright © 2017年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
